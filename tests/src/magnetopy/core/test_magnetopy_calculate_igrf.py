611dde63d20bd891712a636c11f3cc9ccf23a15b
JCBucio <juan.c4@hotmail.com>
2026-04-28 21:44:46 -0600

    Added testing functionality for the calculate-igrf command

--- tests/src/magnetopy/core/test_magnetopy_calculate_igrf.py
+++ tests/src/magnetopy/core/test_magnetopy_calculate_igrf.py
@@ -1,23 +1,23 @@
 from argparse import Namespace
 from logging import getLogger
-
 import os
 import unittest
+
 import pandas as pd
 
-from src.magnetopy.magnetopy_utils.magnetopy_logging import MagnetopyLogging
+from src.magnetopy.magnetopy_core.calculate_igrf import CalculateIGRF
 from src.magnetopy.magnetopy_utils.magnetopy_files_helper import MagnetoPyFilesHelper
-from src.magnetopy.magnetopy_core.diurnal_variation import DiurnalVariation
+from src.magnetopy.magnetopy_utils.magnetopy_logging import MagnetopyLogging
 
 
-class TestDiurnalVariation(unittest.TestCase):
-    def test_diurnal_variation(self):
+class TestCalculateIGRF(unittest.TestCase):
+    def test_calculate_igrf(self):
         """
-        Test the DiurnalVariation class using the test data in the resources/data_examples folder.
+        Test the CalculateIGRF class using the test data in the resources/data_examples folder.
 
         :return: Nothing to return
         """
-        magnetopy_logging: getLogger = MagnetopyLogging().create_magnetopy_logging(logger='TestDiurnalVariation')
+        magnetopy_logging = MagnetopyLogging().create_magnetopy_logging(logger='TestCalculateIGRF')
 
         print("----------------------------------------------------------------------")
 
@@ -25,16 +25,16 @@ class TestDiurnalVariation(unittest.TestCase):
             project_name='cerritos_test',
             stations_file=os.path.abspath('resources/data_examples/cerritos_datos_estaciones.csv'),
             stations_cols='date,time,gpslat,gpslon,magfield',
-            base_station_file=os.path.abspath('resources/data_examples/cerritos_estaciones_base.csv'),
-            base_station_cols='date,time,nT'
+            altitude=1.920,
+            date='2019-03-26'
         )
 
-        DiurnalVariation(arguments=arguments)
+        CalculateIGRF(arguments=arguments)
 
         output_folder = os.path.abspath('resources/cerritos_test')
         output_file = MagnetoPyFilesHelper.most_recent_file(folder_path=output_folder)
         output_file_path = os.path.join(output_folder, output_file)
-        expected_output_file = os.path.abspath('resources/data_examples/cerritos_output.csv')
+        expected_output_file = os.path.abspath('resources/data_examples/cerritos_igrf_output.csv')
 
         output_df = pd.read_csv(output_file_path)
         expected_output_df = pd.read_csv(expected_output_file)
@@ -44,7 +44,10 @@ class TestDiurnalVariation(unittest.TestCase):
         os.remove(output_file_path)
 
         magnetopy_logging.info(f'File "{output_file_path}" deleted successfully.')
-        magnetopy_logging.info('TestDiurnalVariation: test_diurnal_variation passed successfully.')
+        magnetopy_logging.info('TestCalculateIGRF: test_calculate_igrf passed successfully.')
+
 
 if __name__ == '__main__':
     unittest.main()
+
+
